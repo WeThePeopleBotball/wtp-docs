965d5de122abdc0067d572f9b75ee2ada9b17517
ahujaankush <ahujaankush@users.noreply.github.com>
2025-04-07 13:35:19 +0000

    deploy: VentiBotball/venti-lib@8d4b6b0b8d8ecb5077e3be50af0d46badb371759

--- search/all_8.js
+++ search/all_8.js
@@ -6,7 +6,7 @@ var searchData=
   ['improved_5fmotor_2ehpp_3',['improved_motor.hpp',['../improved__motor_8hpp.html',1,'']]],
   ['improved_5fservo_2ecpp_4',['improved_servo.cpp',['../improved__servo_8cpp.html',1,'']]],
   ['improved_5fservo_2ehpp_5',['improved_servo.hpp',['../improved__servo_8hpp.html',1,'']]],
-  ['improvedmotor_6',['ImprovedMotor',['../classImprovedMotor.html',1,'ImprovedMotor'],['../classImprovedMotor.html#a1be3f86eba50d49bd613861e50259d97',1,'ImprovedMotor::ImprovedMotor()']]],
+  ['improvedmotor_6',['ImprovedMotor',['../classImprovedMotor.html',1,'ImprovedMotor'],['../classImprovedMotor.html#a39d3eb4eebf2862b7e7cc732baadbe1f',1,'ImprovedMotor::ImprovedMotor()']]],
   ['improvedservo_7',['ImprovedServo',['../classImprovedServo.html',1,'ImprovedServo'],['../classImprovedServo.html#a9aec317df64ff9bdab871815e7bf8e31',1,'ImprovedServo::ImprovedServo()']]],
   ['is_5fmotor_5fdone_8',['is_motor_done',['../classImprovedMotor.html#aa29f44eeeb8c8f21c13e24516c67b980',1,'ImprovedMotor']]],
   ['is_5fon_5fline_9',['is_on_line',['../classLineManager.html#a537cc298655716c068b49718676fbb3e',1,'LineManager']]],
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['ikcostfunctor_0',['IKCostFunctor',['../structIKCostFunctor.html#acf238f38ac04782465a7a711db66548e',1,'IKCostFunctor']]],
-  ['improvedmotor_1',['ImprovedMotor',['../classImprovedMotor.html#a1be3f86eba50d49bd613861e50259d97',1,'ImprovedMotor']]],
+  ['improvedmotor_1',['ImprovedMotor',['../classImprovedMotor.html#a39d3eb4eebf2862b7e7cc732baadbe1f',1,'ImprovedMotor']]],
   ['improvedservo_2',['ImprovedServo',['../classImprovedServo.html#a9aec317df64ff9bdab871815e7bf8e31',1,'ImprovedServo']]],
   ['is_5fmotor_5fdone_3',['is_motor_done',['../classImprovedMotor.html#aa29f44eeeb8c8f21c13e24516c67b980',1,'ImprovedMotor']]],
   ['is_5fon_5fline_4',['is_on_line',['../classLineManager.html#a537cc298655716c068b49718676fbb3e',1,'LineManager']]]
