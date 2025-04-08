2dc17f474ca4aafa5611f72f8f033acf09845c31
k1W1M4ng0 <k1W1M4ng0@users.noreply.github.com>
2025-04-08 11:44:36 +0000

    deploy: VentiBotball/venti-lib@805eee010a5c3267f822a4e986f07b05321f9fb5

--- main2_8cpp.js
+++ main2_8cpp.js
@@ -1,4 +1,4 @@
 var main2_8cpp =
 [
-    [ "main", "main2_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97", null ]
+    [ "main2", "main2_8cpp.html#a75c2e94360fa8e46050b4f66fe444d6b", null ]
 ];
--- main_8cpp.js
+++ main_8cpp.js
@@ -1,4 +1,5 @@
 var main_8cpp =
 [
-    [ "main", "main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97", null ]
+    [ "main", "main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97", null ],
+    [ "main2", "main_8cpp.html#a75c2e94360fa8e46050b4f66fe444d6b", null ]
 ];
